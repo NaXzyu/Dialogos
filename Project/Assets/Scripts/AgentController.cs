e04bbf6d62f07dbf77281d9179c4f6bc86a8a883
K. R <rawsonkara@gmail.com>
2024-03-18 09:16:44 -0400

    removing for testing

--- Project/Assets/Scripts/AgentController.cs
+++ Project/Assets/Scripts/AgentController.cs
@@ -1,19 +1,9 @@
 using UnityEngine;
-using Unity.Logging;
-using CommandTerminal;
-using Unity.VisualScripting.Antlr3.Runtime;
 
 public class AgentController : MonoBehaviour
 {
-    private Terminal terminal;
-
     void Start()
     {
-        terminal = FindAnyObjectByType<Terminal>();
-        if (terminal == null)
-        {
-            Log.Error("Terminal component not found in the scene.");
-        }
     }
 
     void Update()
@@ -26,23 +16,9 @@ public class AgentController : MonoBehaviour
 
     void ProcessTerminalLog()
     {
-        Log.Info("Process Terminal Log");
-        var _logs = Terminal.Buffer.Logs;
-        var _countX = 0;
-        foreach (var log in _logs)
-        {
-            ++_countX;
-            var _countY = 0;
-            Log.Info("message=" + log.message);
-            var _tokens = log.message.Split(' ');
-            foreach (string _token in _tokens)
-            {
-                ++_countY;
-                Log.Info("token=" + _token);
-                TokenState state = new TokenState(_token);
-                // TODO: Add state to your state management system
-            }
-        }
+        Debug.Log("Process Terminal Log");
+        
+        // TODO process logs
     }
 
     class TokenState
